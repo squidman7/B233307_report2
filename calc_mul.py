ff534170087e17bb973e5bf1996cfde0a2a5770b
squidman7 <akito807233@gmail.com>
2024-01-31 05:43:50 +0900

    add calc_mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -5,11 +5,11 @@ import re
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
+        p = re.compile('\d+')
         if p.match(ai) or p.match(bi):
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+                if 1<=a and b<=999 and a.is_integer() and b.is_integer():
                         valid=True
                 else:
                         valid=False
